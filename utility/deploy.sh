d0cbc4945e60845adadc6747ac70012c7ececc24
isma90 <ismaleiva90@gmail.com>
2019-10-15 19:06:43 -0300

    add wait configuration before start

--- utility/deploy.sh
+++ utility/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd ..
 
@@ -6,13 +6,8 @@ kubectl apply -f 0_dependencies/kube-state-metrics
 kubectl apply -f 0_dependencies/namespace.yaml
 kubectl apply -f 1_general
 kubectl apply -f 2_elastic
-sleep 230s # why? because elastic need a while to run and you need running elastic to connect, without errors, kibana, beats and logstash
 kubectl apply -f 3_kibana
-sleep 30s
 kubectl apply -f 4_beats
-sleep 10s
 kubectl apply -f 5_beats
-sleep 10s
 kubectl apply -f 6_logstash
-sleep 2s
 kubectl create -f 7_fluentd
