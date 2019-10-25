01cd2a8bfa521093781c3660ea17b8853f4ce1e9
isma90 <ismaleiva90@gmail.com>
2019-10-25 13:38:46 -0300

    add sleep

--- utility/deploy.sh
+++ utility/deploy.sh
@@ -7,7 +7,9 @@ kubectl apply -f 0_dependencies/namespace.yaml
 kubectl apply -f 1_general
 kubectl apply -f 2_elastic
 kubectl apply -f 3_kibana
-kubectl apply -f 4_beats
+kubectl apply -f 4_beats/metricbeat-template.yml
+sleep 30s;
+kubectl apply -f 4_beats/metricbeat-dashboards.yml
 kubectl apply -f 5_beats
 kubectl apply -f 6_logstash
 kubectl create -f 7_fluentd
